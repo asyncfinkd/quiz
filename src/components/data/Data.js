6e010a3619ff0942346544b7c238a170d7311128
asyncfinkd <shamgoingzuck@gmail.com>
2021-05-01 16:06:48 +0400

    :rocket: Optimization Data

---
+++ src/components/data/Data.js
@@ -0,0 +1,214 @@
+export const Data = [
+    {
+        id: "0",
+        question:
+          "When you are in a community (at a party with fellow workers or fellow practitioners), you usually prefer",
+        answerOptions: [
+          {
+            answerText: "participate in a general conversation or",
+            isIntrovert: true,
+          },
+          {
+            answerText: "talking with each separately",
+            isIntrovert: false,
+          },
+        ],
+      },
+      {
+        id: "1",
+        question: "You consider yourself more",
+        answerOptions: [
+          {
+            answerText: "realistic or",
+            isIntrovert: true,
+          },
+          {
+            answerText: "prone to theorizing",
+            isIntrovert: false,
+          },
+        ],
+      },
+      {
+        id: "2",
+        question: "In your opinion, which one is worse?",
+        answerOptions: [
+          {
+            answerText: "“living in a dream world”» or",
+            isIntrovert: true,
+          },
+          {
+            answerText: "«all goes according to the plan»",
+            isIntrovert: false,
+          },
+        ],
+      },
+      {
+        id: "3",
+        question: "You are more impressed by",
+        answerOptions: [
+          {
+            answerText: "solid principles or",
+            isIntrovert: true,
+          },
+          {
+            answerText: "strong emotions",
+            isIntrovert: false,
+          },
+        ],
+      },
+      {
+        id: "4",
+        question: "You are more attracted",
+        answerOptions: [
+          {
+            answerText: "persuasive or",
+            isIntrovert: true,
+          },
+          {
+            answerText: "sentimental",
+            isIntrovert: false,
+          },
+        ],
+      },
+      {
+        id: "5",
+        question: "If you have to do an unusual job, it’s more convenient for you.",
+        answerOptions: [
+          {
+            answerText: "to plan it in advance or",
+            isIntrovert: true,
+          },
+          {
+            answerText: "to find out what to do, already in the course of work",
+            isIntrovert: false,
+          },
+        ],
+      },
+      {
+        id: "6",
+        question:
+          "When you need to make a choice or make a decision, you make it. ",
+        answerOptions: [
+          {
+            answerText: "mostly attentive and carefully or",
+            isIntrovert: true,
+          },
+          {
+            answerText: "often spontaneously",
+            isIntrovert: false,
+          },
+        ],
+      },
+      {
+        id: "7",
+        question: "At parties or social gatherings you usually",
+        answerOptions: [
+          {
+            answerText: "stay up late",
+            isIntrovert: true,
+          },
+          {
+            answerText: "leave early feeling tired",
+            isIntrovert: false,
+          },
+        ],
+      },
+      {
+        id: "8",
+        question: "You are more attracted to",
+        answerOptions: [
+          {
+            answerText: "realists or",
+            isIntrovert: true,
+          },
+          {
+            answerText: "imaginative people",
+            isIntrovert: false,
+          },
+        ],
+      },
+      {
+        id: "9",
+        question: "You are more interested in",
+        answerOptions: [
+          {
+            answerText: "what really exists or",
+            isIntrovert: true,
+          },
+          {
+            answerText: "unrealized opportunities",
+            isIntrovert: false,
+          },
+        ],
+      },
+      {
+        id: "10",
+        question: "Your conclusions about people based",
+        answerOptions: [
+          {
+            answerText: "on rules more often than on circumstances or",
+            isIntrovert: true,
+          },
+          {
+            answerText: "on circumstances more often than on rules",
+            isIntrovert: false,
+          },
+        ],
+      },
+      {
+        id: "11",
+        question: "When assessing other people, you usually",
+        answerOptions: [
+          {
+            answerText: "firm and objective",
+            isIntrovert: true,
+          },
+          {
+            answerText: "condescending and subjective",
+            isIntrovert: false,
+          },
+        ],
+      },
+      {
+        id: "12",
+        question: "More often you act",
+        answerOptions: [
+          {
+            answerText: "punctually or",
+            isIntrovert: true,
+          },
+          {
+            answerText: "slowly",
+            isIntrovert: false,
+          },
+        ],
+      },
+      {
+        id: "13",
+        question: "You prefer",
+        answerOptions: [
+          {
+            answerText: "doing work in advance or",
+            isIntrovert: true,
+          },
+          {
+            answerText: "doing everything last moment",
+            isIntrovert: false,
+          },
+        ],
+      },
+      {
+        id: "14",
+        question: "Amoung your friends you",
+        answerOptions: [
+          {
+            answerText: "you know everything about everyone or",
+            isIntrovert: true,
+          },
+          {
+            answerText: "the last to know what is happening",
+            isIntrovert: false,
+          },
+        ],
+      },
+];
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,222 +4,7 @@ import { withRouter } from "react-router-dom";
 // import List from "./list/List";
 import { Progress } from "antd";
 import "../../stylesheet/hero/hero.css";
-
-
-const questions = [
-  {
-    id: "0",
-    question:
-      "When you are in a community (at a party with fellow workers or fellow practitioners), you usually prefer",
-    answerOptions: [
-      {
-        answerText: "participate in a general conversation or",
-        isIntrovert: true,
-      },
-      {
-        answerText: "talking with each separately",
-        isIntrovert: false,
-      },
-    ],
-  },
-  {
-    id: "1",
-    question: "You consider yourself more",
-    answerOptions: [
-      {
-        answerText: "realistic or",
-        isIntrovert: true,
-      },
-      {
-        answerText: "prone to theorizing",
-        isIntrovert: false,
-      },
-    ],
-  },
-  {
-    id: "2",
-    question: "In your opinion, which one is worse?",
-    answerOptions: [
-      {
-        answerText: "“living in a dream world”» or",
-        isIntrovert: true,
-      },
-      {
-        answerText: "«all goes according to the plan»",
-        isIntrovert: false,
-      },
-    ],
-  },
-  {
-    id: "3",
-    question: "You are more impressed by",
-    answerOptions: [
-      {
-        answerText: "solid principles or",
-        isIntrovert: true,
-      },
-      {
-        answerText: "strong emotions",
-        isIntrovert: false,
-      },
-    ],
-  },
-  {
-    id: "4",
-    question: "You are more attracted",
-    answerOptions: [
-      {
-        answerText: "persuasive or",
-        isIntrovert: true,
-      },
-      {
-        answerText: "sentimental",
-        isIntrovert: false,
-      },
-    ],
-  },
-  {
-    id: "5",
-    question: "If you have to do an unusual job, it’s more convenient for you.",
-    answerOptions: [
-      {
-        answerText: "to plan it in advance or",
-        isIntrovert: true,
-      },
-      {
-        answerText: "to find out what to do, already in the course of work",
-        isIntrovert: false,
-      },
-    ],
-  },
-  {
-    id: "6",
-    question:
-      "When you need to make a choice or make a decision, you make it. ",
-    answerOptions: [
-      {
-        answerText: "mostly attentive and carefully or",
-        isIntrovert: true,
-      },
-      {
-        answerText: "often spontaneously",
-        isIntrovert: false,
-      },
-    ],
-  },
-  {
-    id: "7",
-    question: "At parties or social gatherings you usually",
-    answerOptions: [
-      {
-        answerText: "stay up late",
-        isIntrovert: true,
-      },
-      {
-        answerText: "leave early feeling tired",
-        isIntrovert: false,
-      },
-    ],
-  },
-  {
-    id: "8",
-    question: "You are more attracted to",
-    answerOptions: [
-      {
-        answerText: "realists or",
-        isIntrovert: true,
-      },
-      {
-        answerText: "imaginative people",
-        isIntrovert: false,
-      },
-    ],
-  },
-  {
-    id: "9",
-    question: "You are more interested in",
-    answerOptions: [
-      {
-        answerText: "what really exists or",
-        isIntrovert: true,
-      },
-      {
-        answerText: "unrealized opportunities",
-        isIntrovert: false,
-      },
-    ],
-  },
-  {
-    id: "10",
-    question: "Your conclusions about people based",
-    answerOptions: [
-      {
-        answerText: "on rules more often than on circumstances or",
-        isIntrovert: true,
-      },
-      {
-        answerText: "on circumstances more often than on rules",
-        isIntrovert: false,
-      },
-    ],
-  },
-  {
-    id: "11",
-    question: "When assessing other people, you usually",
-    answerOptions: [
-      {
-        answerText: "firm and objective",
-        isIntrovert: true,
-      },
-      {
-        answerText: "condescending and subjective",
-        isIntrovert: false,
-      },
-    ],
-  },
-  {
-    id: "12",
-    question: "More often you act",
-    answerOptions: [
-      {
-        answerText: "punctually or",
-        isIntrovert: true,
-      },
-      {
-        answerText: "slowly",
-        isIntrovert: false,
-      },
-    ],
-  },
-  {
-    id: "13",
-    question: "You prefer",
-    answerOptions: [
-      {
-        answerText: "doing work in advance or",
-        isIntrovert: true,
-      },
-      {
-        answerText: "doing everything last moment",
-        isIntrovert: false,
-      },
-    ],
-  },
-  {
-    id: "14",
-    question: "Amoung your friends you",
-    answerOptions: [
-      {
-        answerText: "you know everything about everyone or",
-        isIntrovert: true,
-      },
-      {
-        answerText: "the last to know what is happening",
-        isIntrovert: false,
-      },
-    ],
-  },
-];
+import { Data } from "../data/Data";
 
 class Hero extends React.Component {
   constructor(props) {
@@ -234,21 +19,20 @@ class Hero extends React.Component {
     showResult: false,
     history: [],
     showButtons: true,
+    questions: Data,
   };
   nextQuestion = (isCorrect) => {
     if (isCorrect === true) {
       this.setState((state) => ({
         introvertCounter: state.introvertCounter + 1,
       }));
-      console.log("introvert" + this.state.introvertCounter);
     } else {
       this.setState((state) => ({
         extravertCounter: state.extravertCounter + 1,
       }));
-      console.log("extravert", this.state.extravertCounter);
     }
     const nextQuestion = this.state.currentQuestion + 1;
-    if (nextQuestion < questions.length) {
+    if (nextQuestion < this.state.questions.length) {
       this.setState((state) => ({
         currentQuestion: (state.currentQuestion = nextQuestion),
       }));
@@ -258,17 +42,17 @@ class Hero extends React.Component {
       }));
     }
 
-    this.state.history.push(questions[this.state.currentQuestion].id);
-    questions.map((item) => {
+    this.state.history.push(
+      this.state.questions[this.state.currentQuestion].id
+    );
+    this.state.questions.map((item) => {
       this.state.history.map((item2) => {
         if (item.id == item2) {
           document.getElementById(`${item.id}`).style.backgroundColor = "green";
           document.getElementById(`${item.id}`).style.color = "white";
         }
       });
-      // console.log(item);
     });
-    // console.log(this.state.history);
   };
 
   prevQuestion = () => {
@@ -312,7 +96,7 @@ class Hero extends React.Component {
   rendeHero = () => {
     const username = localStorage.getItem("username");
     const renderPercent = Math.round(
-      (this.state.currentQuestion / questions.length) * 100
+      (this.state.currentQuestion / this.state.questions.length) * 100
     );
     if (username == "" || !username) {
       this.props.history.push("/");
@@ -322,7 +106,7 @@ class Hero extends React.Component {
           <div className="flex">
             <div className="list-container">
               <ul>
-                {questions.map((item) => {
+                {this.state.questions.map((item) => {
                   return (
                     <>
                       <li
@@ -330,7 +114,7 @@ class Hero extends React.Component {
                           this.setState((state) => ({
                             currentQuestion: (state.currentQuestion = +(+item.id)),
                           }));
-                          questions.map((item2) => {
+                          this.state.questions.map((item2) => {
                             if (item2.id < +item.id) {
                               document.getElementById(
                                 `${item2.id}`
@@ -372,12 +156,15 @@ class Hero extends React.Component {
                         <Progress percent={renderPercent} status="active" />
                         <div aria-label="Question Length" className="mt-3">
                           <span className="questionLength-span">
-                            {`Question ${this.state.currentQuestion}/${questions.length}`}
+                            {`Question ${this.state.currentQuestion}/${this.state.questions.length}`}
                           </span>
                         </div>
                         <div aria-label="Question" className="mt-3">
                           <span className="question-span">
-                            {questions[this.state.currentQuestion].question}
+                            {
+                              this.state.questions[this.state.currentQuestion]
+                                .question
+                            }
                           </span>
                         </div>
                         <div
@@ -386,7 +173,7 @@ class Hero extends React.Component {
                         >
                           {this.state.showButtons && (
                             <>
-                              {questions[
+                              {this.state.questions[
                                 this.state.currentQuestion
                               ].answerOptions.map((item) => {
                                 return (
