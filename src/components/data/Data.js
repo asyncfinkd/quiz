afe2077f25fe7d869f90f6298bc6aac4cd001b5d
asyncfinkd <shamgoingzuck@gmail.com>
2021-05-01 23:18:19 +0400

    Update Data.js

--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -230,4 +230,281 @@ export const Data = [
       },
     ],
   },
+  {
+    id: "19",
+    question: "You are more attracted to situations",
+    answerOptions: [
+      {
+        answerText: "in which there is an element of certainty",
+      },
+      {
+        answerText: "which are full of unpredictability",
+      },
+    ],
+  },
+  {
+    id: "20",
+    question: "You can say that you are more",
+    answerOptions: [
+      {
+        answerText: "serious and focused",
+      },
+      {
+        answerText: "not taking life too seriously",
+      },
+    ],
+  },
+  {
+    id: "21",
+    question: "When talking on the phone",
+    answerOptions: [
+      {
+        answerText: "you rarely think about: “What to say?” in advance Or",
+      },
+      {
+        answerText: "often think out in advance what you will say",
+      },
+    ],
+  },
+  {
+    id: "22",
+    question: "How do you think? Data",
+    answerOptions: [
+      {
+        answerText: '"speak for itself" or',
+      },
+      {
+        answerText: "illustrates some patterns",
+      },
+    ],
+  },
+  {
+    id: "23",
+    question: "Dreamers",
+    answerOptions: [
+      {
+        answerText: "annoy you or",
+      },
+      {
+        answerText: "attract and fascinate you",
+      },
+    ],
+  },
+  {
+    id: "24",
+    question: "More often you are",
+    answerOptions: [
+      {
+        answerText: "calm and impartial or",
+      },
+      {
+        answerText: "cordial and sympathetic",
+      },
+    ],
+  },
+  {
+    id: "25",
+    question: "You think that it is worse to",
+    answerOptions: [
+      {
+        answerText: "be unreasonable or",
+      },
+      {
+        answerText: "be malevolent",
+      },
+    ],
+  },
+  {
+    id: "26",
+    question: "In most cases it is better to",
+    answerOptions: [
+      {
+        answerText: "try to control the course of events or",
+      },
+      {
+        answerText: "rely on the natural course of events.",
+      },
+    ],
+  },
+  {
+    id: "27",
+    question: "You feel better when",
+    answerOptions: [
+      {
+        answerText: "you have already made a purchase or",
+      },
+      {
+        answerText: "you still have the opportunity to do it.",
+      },
+    ],
+  },
+  {
+    id: "28",
+    question: "In the group of colleagues you tend to",
+    answerOptions: [
+      {
+        answerText: "initiate a conversation or",
+      },
+      {
+        answerText: "wait for someone to contact you",
+      },
+    ],
+  },
+  {
+    id: "29",
+    question: "Claims made on the basis of common sense",
+    answerOptions: [
+      {
+        answerText: "rarely raise doubts or",
+      },
+      {
+        answerText: "often raise doubts.",
+      },
+    ],
+  },
+  {
+    id: "30",
+    question: "Would you like your friend to be a person",
+    answerOptions: [
+      {
+        answerText: 'who "stands firmly on the ground" or',
+      },
+      {
+        answerText: "who always has new ideas.",
+      },
+    ],
+  },
+  {
+    id: "31",
+    question: "When making decisions it is easier for you to follow",
+    answerOptions: [
+      {
+        answerText: "the rules and standards or",
+      },
+      {
+        answerText: "the wishes of interested parties",
+      },
+    ],
+  },
+  {
+    id: "32",
+    question: "You are rather a person",
+    answerOptions: [
+      {
+        answerText: "hard than soft or",
+      },
+      {
+        answerText: "soft than hard",
+      },
+    ],
+  },
+  {
+    id: "33",
+    question: "You are more attracted by the opportunity",
+    answerOptions: [
+      {
+        answerText: "to organize something or",
+      },
+      {
+        answerText: "creatively use the available opportunities",
+      },
+    ],
+  },
+  {
+    id: "34",
+    question: "In a situation you value",
+    answerOptions: [
+      {
+        answerText: "clarity or",
+      },
+      {
+        answerText: "the possibility of unpredictable turns.",
+      },
+    ],
+  },
+  {
+    id: "35",
+    question: "Meeting new and unusual people.",
+    answerOptions: [
+      {
+        answerText: "stimulates and energizes you or",
+      },
+      {
+        answerText: "tires you",
+      },
+    ],
+  },
+  {
+    id: "36",
+    question: "In most cases you are",
+    answerOptions: [
+      {
+        answerText: "practical or",
+      },
+      {
+        answerText: "a dreamer",
+      },
+    ],
+  },
+  {
+    id: "37",
+    question: "You are more interested to know",
+    answerOptions: [
+      {
+        answerText: "wheather other people mayt be helpful to you or",
+      },
+      {
+        answerText: "other people's point of view",
+      },
+    ],
+  },
+  {
+    id: "38",
+    question: "You receive more satisfaction",
+    answerOptions: [
+      {
+        answerText: "through a discussion of the issue or",
+      },
+      {
+        answerText: "through reaching agreement on the subject matter.",
+      },
+    ],
+  },
+  {
+    id: "39",
+    question: "Your actions are more governed by",
+    answerOptions: [
+      {
+        answerText: "your head or",
+      },
+      {
+        answerText: "your heart",
+      },
+    ],
+  },
+  {
+    id: "40",
+    question:
+      "When you know that at a certain time you will be engaged in a certain business",
+    answerOptions: [
+      {
+        answerText: "you are glad that you can plan your time or",
+      },
+      {
+        answerText: "you don't like when you have to follow a certain plan",
+      },
+    ],
+  },
+  {
+    id: "41",
+    question: "Usually you",
+    answerOptions: [
+      {
+        answerText: "strive to achieve desired results or",
+      },
+      {
+        answerText: "are satisfied with what comes out",
+      },
+    ],
+  },
 ];
