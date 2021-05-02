6331a0a621951da0347fabd839989c797e5b7950
asyncfinkd <shamgoingzuck@gmail.com>
2021-05-02 22:28:12 +0400

    :rocket: Ignore

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -99,6 +99,14 @@ class Hero extends React.Component {
                         `${item2.id}`
                       ).style.backgroundColor = "gray";
                     }
+                    if (
+                      item2.id ==
+                      this.state.questions[this.state.currentQuestion + 1].id
+                    ) {
+                      document.getElementById(
+                        `${item.id}`
+                      ).style.backgroundColor = "#0d6efd";
+                    }
                   });
                   this.state.history.map((item2) => {
                     document.getElementById(`${+item2}`).style.backgroundColor =
