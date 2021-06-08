a56c7f5abb1534ca823cc112f9b71e924f77c8b5
asyncfinkd <shamgoingzuck@gmail.com>
2021-06-08 22:04:33 +0400

    :rocket: Email Sent

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
+const nodemailer = require("nodemailer");
 
 app.use(cors());
 app.use(express.json());
--- server/router/User/Insert/UserInsert.js
+++ server/router/User/Insert/UserInsert.js
@@ -1,16 +1,44 @@
 const router = require("express").Router();
 const UserSchema = require("../../../schema/User/User");
+const nodemailer = require("nodemailer");
 
 router.route("/insertUser").post(async (req, res) => {
   const username = req.body.username;
+  const result = req.body.result;
 
   const insertUserSchema = new UserSchema({
     username: username,
-    result: req.body.result,
+    result: result,
   });
 
   insertUserSchema.save();
   res.json("insert");
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "billgatesprogrammer@gmail.com",
+      pass: "nikanikonika",
+    },
+  });
+
+  let mailOptions = {
+    from: "billgatesprogrammer@gmail.com",
+    to: `${username}`,
+    subject: "Testing and Testing",
+    text: `Hello, This is your Result:
+${result.map((item) => {
+return "Question: " + item.question + "\n" + "Answer: " + item.value + "\n";
+})}`
+  };
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log("error occurs");
+    } else {
+      console.log("email sent");
+    }
+  });
 });
 
 module.exports = router;
