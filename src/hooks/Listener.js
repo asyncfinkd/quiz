6dfeb7b4c497f77d4471e0d60b942af9aaafcc68
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-06-16 03:56:29 -0700

    Add files via upload

--- src/hooks/Listener.js
+++ src/hooks/Listener.js
@@ -1,22 +1,22 @@
 import Swal from "sweetalert2";
 import { SentResult } from "../auth/SentResult/SentResultAuth";
 
-export function SentEmail(language, email, result) {
-    if(!email) {
-        if(language === "English") {
+export function SentEmail(language, email, result, points) {
+    if (!email) {
+        if (language === "English") {
             Swal.fire({
                 icon: "error",
                 title: "Oops...",
                 text: "Please enter a email!",
-              });
+            });
         } else {
             Swal.fire({
                 icon: "error",
                 title: "Ой...",
                 text: "Пожалуйста, введите адрес электронной почты!",
-              });
+            });
         }
     } else {
-        SentResult(language, email, result);
+        SentResult(language, email, result, points);
     }
 }
